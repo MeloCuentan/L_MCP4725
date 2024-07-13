6f2230586c4273150373fdef898126df0c041aa8
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2024-07-13 12:51:59 +0100

    Update L_MCP4725.h

--- src/L_MCP4725.h
+++ src/L_MCP4725.h
@@ -1,11 +1,12 @@
 #ifndef L_MCP4725_H
 #define L_MCP4725_H
 
+#include <Arduino.h>
 #include <Wire.h>
 
-class MCP4725 {
-  public:
-    MCP4725();
+class L_MCP4725 {
+public:
+    L_MCP4725();
     void begin(uint8_t I2C_ADDR = 0x60);
     
     void setVoltageRange(float minVoltage = 0.0, float maxVoltage = 5.0);
@@ -16,10 +17,10 @@ class MCP4725 {
     void setOutputValue(uint16_t value);
     uint16_t getOutputValue();
 
-  private:
+private:
     float _minVoltage;
     float _maxVoltage;
-    uint8_t _MCP4725_ADDRESS;
+    uint8_t _I2C_ADDR;
     uint16_t voltageToDAC(float voltage);
     float dacToVoltage(uint16_t value);
     void writeDAC(uint16_t value);
