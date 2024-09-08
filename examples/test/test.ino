d07b560ad0db60cd59a75e35a8187ce3fed1be21
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2024-09-08 10:31:04 +0100

    Update test.ino

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -1,32 +1,45 @@
 #include <Wire.h>
 #include "L_MCP4725.h"
 
-MCP4725 dac;
+float VCCreal = 3.97; // idealmente son 5V
+
+L_MCP4725 dac(0x60); // Dirección I2C predeterminada del MCP4725
 
 void setup() {
-  dac.begin();
+    Serial.begin(9600);
 
-  dac.setVoltageRange(0.0, 5.0); // Valores en voltios
-  //dac.setValueRange(0, 4095);  // Valores en RAW
+    dac.setVoltageRange(0.0, VCCreal); // Configurar rango de voltaje
+    dac.setValueRange(0, 4095);  // Configurar rango de valores DAC
+
+    Serial.println("\nConfigurando valor del DAC a 1000");
+    dac.setOutputValue(1000); // establece el valor del dac en 1000 (valor RAW)
+  //  dac.setEEPROMValue(2000); // Guardamos en la eeprom el valor especificado
+    delay(100); // Dar tiempo para que el valor se establezca
+
+    uint16_t outputValue = dac.getOutputValue(); // Leer el valor del DAC
+    Serial.print("Valor leído: ");
+    Serial.println(outputValue); // Imprimir el valor leído
 }
 
 void loop() {
-  // Establecer el voltaje de salida a 1.5V
-  dac.setOutputVoltage(1.5); // Valor en voltios
-  delay(1000);
+  if(Serial.available() != 0) {
+    String rx = Serial.readStringUntil('\n');
+    uint16_t valor = rx.toInt();
+    cambiarValor(valor);
+  }
+}
 
+void cambiarValor(uint16_t valor) {
+  if (valor > 4095) {
+    Serial.println("Valor fuera de rango");
+    return;
+  }
+//  Serial.print("Ajustar valor a :" + String(valor));
+//  dac.setOutputValue(valor); // Configurar valor del DAC
+//  Serial.print("    Valor leído: " + String(dac.getOutputValue()));
 
-  // Obtener el valor de salida del DAC
-  uint16_t dacValue = dac.getOutputValue(); // Devuelve el valor RAW
-  Serial.print("Valor de salida del DAC: ");
-  Serial.println(dacValue);
-
-  // Convertir el valor del DAC a voltaje
-  float outputVoltage = dac.getOutputVoltage();
-  Serial.print("Voltaje de salida: ");
-  Serial.println(outputVoltage);
-
-  // Establecer el valor de salida del DAC directamente a la mitad del rango
-  dac.setOutputValue(2048);
-  delay(1000);
+  float valorConvertido = float(valor) / 100.0;
+  Serial.print("Ajustar voltios a :" + String(valorConvertido));
+  dac.setOutputVoltage(valorConvertido);
+  Serial.println("    Voltaje leído: " + String(dac.getOutputVoltage()));
 }
