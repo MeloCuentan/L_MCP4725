3a7a29cd50f7a22cefd6a3c41456960403afbe8c
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2024-09-08 10:25:16 +0100

    Update L_MCP4725.cpp

--- src/L_MCP4725.cpp
+++ src/L_MCP4725.cpp
@@ -4,7 +4,7 @@ L_MCP4725::L_MCP4725(uint8_t I2C_ADDR) {
   _I2C_ADDR = I2C_ADDR;
   Wire.begin();
 }
-
+// *********************************   SECCIÓN PARA TRABAJAR CON VALORES   *********************************
 void L_MCP4725::setValueRange(uint16_t minValue, uint16_t maxValue) {
   if (minValue >= maxValue || maxValue > 4095) return;
   _minVoltage = dacToVoltage(minValue);
@@ -12,21 +12,21 @@ void L_MCP4725::setValueRange(uint16_t minValue, uint16_t maxValue) {
 }
 
 void L_MCP4725::setOutputValue(uint16_t value) {
-  if (value > 4095) value = 4095;  // Asegurarse de que no exceda el valor máximo de 12 bits
+  if (value > 4095) value = 4095;  // Asegurar que no exceda el valor máximo de 12 bits
   writeDAC(value);
 }
 
 uint16_t L_MCP4725::getOutputValue() {
-  Wire.requestFrom((uint8_t)_I2C_ADDR, (uint8_t)3);  // Pedir 2 bytes desde la dirección I2C
+  Wire.requestFrom((uint8_t)_I2C_ADDR, (uint8_t)3);  // Pedir 3 bytes a la dirección I2C
 
-  uint32_t startMillis = millis();
-  while (Wire.available() < 3) {
+  uint32_t startMillis = millis();        // Control de tiempo para evitar bloqueo de funcionamiento
+  while (Wire.available() < 3) {          // Esperar a recibir 3 bytes
     if (millis() - startMillis >= 100) {  // 100 ms timeout
-      return 0;                           // o algún valor de error apropiado
+      return 0;                           // Retorna 0 si no ha leído en el tiempo especificado
     }
   }
 
-  uint8_t readCommand = Wire.read();  // Leer el primer byte. Este no se utilziará
+  uint8_t readCommand = Wire.read();  // Leer el primer byte (no se utilizará)
   uint8_t msb = Wire.read();          // Leer el segundo byte
   uint8_t lsb = Wire.read();          // Leer el tercer byte
 
@@ -43,6 +43,7 @@ void L_MCP4725::setEEPROMValue(uint16_t value) {
   Wire.endTransmission();
 }
 
+// *********************************   SECCIÓN PARA TRABAJAR CON VOLTAJE   *********************************
 void L_MCP4725::setVoltageRange(float minVoltage, float maxVoltage) {
   if (minVoltage >= maxVoltage || minVoltage < 0.0 || maxVoltage > 5.0) return;
   _minVoltage = minVoltage;
@@ -65,6 +66,7 @@ void L_MCP4725::setEEPROMVoltage(float voltage) {
   setEEPROM(uint16_t(value));
 }
 
+// *********************************    SECCIÓN PARA FUNCIONES PRIVADAS    *********************************
 void L_MCP4725::writeDAC(uint16_t value) {
   Wire.beginTransmission(_I2C_ADDR);
   Wire.write((value >> 8) & 0x0F);  // Enviar los 4 bits más significativos
