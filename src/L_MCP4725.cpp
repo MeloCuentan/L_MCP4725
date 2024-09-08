4493fd85dfde206623dbf97cf48fab0480330a1d
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2024-09-08 09:29:34 +0100

    Update L_MCP4725.cpp

--- src/L_MCP4725.cpp
+++ src/L_MCP4725.cpp
@@ -6,13 +6,15 @@ L_MCP4725::L_MCP4725(uint8_t I2C_ADDR) {
 }
 
 void L_MCP4725::setVoltageRange(float minVoltage, float maxVoltage) {
-  if (minVoltage > 0.0 && minVoltage < maxVoltage) _minVoltage = minVoltage;
-  if (maxVoltage > minVoltage && maxVoltage < 5.0) _maxVoltage = maxVoltage;
+  if (minVoltage >= maxVoltage || minVoltage < 0.0 || maxVoltage > 5.0) return;
+  _minVoltage = minVoltage;
+  _maxVoltage = maxVoltage;
 }
 
 void L_MCP4725::setValueRange(uint16_t minValue, uint16_t maxValue) {
-  if (minValue >= 0 && minValue < maxValue) _minVoltage = dacToVoltage(minValue);
-  if (maxValue > minValue && maxValue < 4095) _maxVoltage = dacToVoltage(maxValue);
+  if (minValue >= maxValue || maxValue > 4095) return;
+  _minVoltage = dacToVoltage(minValue);
+  _maxVoltage = dacToVoltage(maxValue);
 }
 
 void L_MCP4725::setOutputVoltage(float voltage) {
@@ -41,8 +43,8 @@ void L_MCP4725::writeDAC(uint16_t value) {
 uint16_t L_MCP4725::getOutputValue() {
   Wire.requestFrom((uint8_t)_I2C_ADDR, (uint8_t)3);  // Pedir 2 bytes desde la dirección I2C
 
-  uint32_t startMillis = millis();
-  while (Wire.available() < 3) {
+  uint32_t startMillis = millis();        // Contador para evitar que quede en un bucle
+  while (Wire.available() < 3) {          // Mientras no se reciban 3 bytes
     if (millis() - startMillis >= 100) {  // 100 ms timeout
       return 0;                           // o algún valor de error apropiado
     }
