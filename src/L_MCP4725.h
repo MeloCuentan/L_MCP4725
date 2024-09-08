08e4b2dc1bb580a0b231b78524660a65e8ea839e
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2024-09-08 10:12:25 +0100

    Update L_MCP4725.h

--- src/L_MCP4725.h
+++ src/L_MCP4725.h
@@ -8,13 +8,16 @@ class L_MCP4725 {
 public:
     L_MCP4725(uint8_t I2C_ADDR = 0x60);
     
-    void setVoltageRange(float minVoltage = 0.0, float maxVoltage = 5.0);
-    void setOutputVoltage(float voltage);
-    float getOutputVoltage();
-
     void setValueRange(uint16_t minValue = 0, uint16_t maxValue = 4095);
     void setOutputValue(uint16_t value);
     uint16_t getOutputValue();
+    void setEEPROM(uint16_t value);
+    
+    void setVoltageRange(float minVoltage = 0.0, float maxVoltage = 5.0);
+    void setOutputVoltage(float voltage);
+    float getOutputVoltage();
+    void setEEPROM(float voltage);
+
 
 private:
     float _minVoltage = 0.0;
